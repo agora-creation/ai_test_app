990a9f471973dae255b8782a775eea330e6ff394
shima <shimamura@agora-c.com>
2025-05-12 15:58:27 +0900

    20250512 commit

--- lib/services/ad.dart
+++ lib/services/ad.dart
@@ -15,10 +15,10 @@ class AdService {
 
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/6300978111'; // テストID
+      // return 'ca-app-pub-3940256099942544/6300978111'; // テストID
       return 'ca-app-pub-9791675225952080/4997881045';
     } else if (Platform.isIOS) {
-      return 'ca-app-pub-3940256099942544/6300978111'; // テストID
+      // return 'ca-app-pub-3940256099942544/6300978111'; // テストID
       return 'ca-app-pub-9791675225952080/7711674293';
     } else {
       throw UnsupportedError('Unsupported platform');
